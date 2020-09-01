7014374114aaaf8dcf1e236b2a81c9ce5c1d7ace
BlommaWalla <lutz.hahlbeckmail.com>
2020-09-01 13:06:34 +0200

    gukyufgiugftuoofotf

---
+++ guten_morgen/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace guten_morgen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Guten Morgen");
+            Console.ReadLine();
+        }
+    }
+}
