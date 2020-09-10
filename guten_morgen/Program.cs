87f18ff801544a39122c368aaf99adb0e483fc30
BlommaWalla <lutz.hahlbeckmail.com>
2020-09-10 16:24:11 +0200

    class test o sånt

--- guten_morgen/Program.cs
+++ guten_morgen/Program.cs
@@ -6,7 +6,22 @@ namespace guten_morgen
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Guten Morgen");
+
+            string[] color = {"lmao", "dab", "gluten"};
+            int[] cool = {1, 2, 3};
+            float[] thicc = {0.1f, 0.2f, 47f};
+            
+            Sak[] saker = new Sak[3];
+
+
+            for (int i = 0; i < saker.Length; i++) {
+                saker[i] = new Sak() {coolnes = cool[i], largenes = thicc[i], colornes = color[i]};
+        
+                System.Console.WriteLine(saker[i].coolnes + " " + saker[i].largenes + " " + saker[i].colornes);   
+
+            }
+
+
             Console.ReadLine();
         }
     }
---
+++ guten_morgen/sak.cs
@@ -0,0 +1,5 @@
+public class Sak {
+        public int coolnes;
+        public float largenes;
+        public string colornes;
+    }
